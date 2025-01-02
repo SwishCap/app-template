7d3961322daa8f542b20e52142c65d8aadfdd51a
Flick <techandart100@gmail.com>
2025-01-02 10:47:32 -0800

    config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    reactStrictMode: true,
     images: {
         unoptimized: true,
     },
+    // This is important for IPFS hosting
+    assetPrefix: './',
     trailingSlash: true,
 };
 
